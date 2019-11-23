d7553f6fcb52dbc6991dd0aae56db6f71bb97523
mfernandez1047 <mfernandez21135@columbushs.com>
2019-11-22 21:36:37 -0500

    finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -63,14 +63,38 @@ function orderAlphabetically(array){
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(array) {
-    if(array.length == 0) {
-        return 0;
+
+    if (array.length == 0) {
+        return 0
     }
+
+
+    let totalRates = array.reduce((a, c) => {
+
+        return a + (c.rate ? c.rate : 0);
+
+    }, 0);
+
+
+    let avgRate = (totalRates / array.length).toFixed(2);
+
+    return Number(avgRate);
+
 }
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(array) {
 
+    let dramaMovies = array.filter(eachMovie => {
+        return eachMovie.genre.includes("Drama")
+    });
+
+    let avgDramaMovies = ratesAverage(dramaMovies);
+
+    return avgDramaMovies;
+
 }
+
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(array) {
@@ -93,7 +117,7 @@ function turnHoursToMinutes(array) {
             final = (hour * 60 + min);
 
         }
-        
+
         else if (duration.includes('h')) {
             hour = Number(duration.substring(0, duration.indexOf("h")));
             final = hour * 60;
